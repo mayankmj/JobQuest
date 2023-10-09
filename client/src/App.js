a0eecb6aee58cdee739494763556ce932c9c0b91
mayankmj <mayankjohari877@gmail.com>
2023-10-10 03:09:03 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+       {/* // left section */}
+         <div>
+           <div>
+
+           </div>
+
+           <div>
+
+           </div>
+         </div>
+
+         {/* right section */}
+         <div>
+
+         </div>
+    </div>
+  );
+}
+
+export default App;
